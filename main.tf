f4b45d860f79b71a2cb3c990de54c082ea484233
aromanov <aromanov@inno.tech>
2022-01-31 02:36:18 +0300

    add backend

--- main.tf
+++ main.tf
@@ -25,10 +25,25 @@ resource "digitalocean_database_user" "user" {
   name       = var.database_user
 }
 
+resource "digitalocean_database_connection_pool" "connection_pool" {
+  cluster_id = digitalocean_database_cluster.postgres.id
+  db_name    = var.database_name
+  mode       = "transaction"
+  name       = "${var.project_name}-connection-pool"
+  size       = 10
+  user       = var.database_user
+  depends_on = [
+    digitalocean_database_cluster.postgres,
+    digitalocean_database_db.database,
+    digitalocean_database_user.user
+  ]
+}
+
 resource "digitalocean_app" "application" {
   spec {
     name   = "${var.project_name}-app"
     region = var.backend.region
+
     domain {
       name = "${var.project_name}.${var.domain}"
       zone = var.domain
@@ -36,7 +51,10 @@ resource "digitalocean_app" "application" {
     }
 
     static_site {
-      name = "frontend"
+      name           = "frontend"
+      build_command  = "yarn build"
+      output_dir     = "build"
+      index_document = "index.html"
 
       github {
         repo           = "Romanow/frontend-todo-list"
@@ -45,7 +63,7 @@ resource "digitalocean_app" "application" {
       }
 
       env {
-        key = "REACT_APP_BACKEND_IP"
+        key   = "REACT_APP_BACKEND_IP"
         value = "/backend"
       }
 
@@ -74,17 +92,17 @@ resource "digitalocean_app" "application" {
 
       env {
         key   = "DATABASE_URL"
-        value = digitalocean_database_cluster.postgres.host
+        value = digitalocean_database_connection_pool.connection_pool.host
       }
 
       env {
         key   = "DATABASE_PORT"
-        value = digitalocean_database_cluster.postgres.port
+        value = digitalocean_database_connection_pool.connection_pool.port
       }
 
       env {
         key   = "DATABASE_NAME"
-        value = var.database_name
+        value = digitalocean_database_connection_pool.connection_pool.name
       }
 
       env {
@@ -94,12 +112,12 @@ resource "digitalocean_app" "application" {
 
       env {
         key   = "DATABASE_PASSWORD"
-        value = digitalocean_database_user.user.password
+        value = digitalocean_database_connection_pool.connection_pool.password
         type  = "SECRET"
       }
 
       health_check {
-        http_path             = "/backend/manage/health"
+        http_path             = "/manage/health"
         initial_delay_seconds = 20
         period_seconds        = 5
         success_threshold     = 1
@@ -110,13 +128,5 @@ resource "digitalocean_app" "application" {
         path = "/backend"
       }
     }
-  }
-}
-
-resource "digitalocean_database_firewall" "firewall" {
-  cluster_id = digitalocean_database_cluster.postgres.id
-  rule {
-    type  = "app"
-    value = digitalocean_app.application.id
   }
 }
